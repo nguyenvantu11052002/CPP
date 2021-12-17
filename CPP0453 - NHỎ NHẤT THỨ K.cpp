04acd64f220e10cefb05c928c44aae69de1791d2
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2021-12-17 22:03:56 +0700

    Create CPP0453 - NHỎ NHẤT THỨ K.cpp

---
+++ CPP0453 - NHỎ NHẤT THỨ K.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+void inraketqua(){
+	int n, k;
+	cin >> n >> k;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end());
+	cout << a[k - 1] << endl;
+}
+
+int main(){
+	int k;
+	cin >> k;
+	while(k--){
+		inraketqua();
+	}
+	return 0;
+}

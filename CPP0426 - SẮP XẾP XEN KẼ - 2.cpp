ff0b3d129c3e85428443b5babeaed7521c3bba4f
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2021-12-19 11:33:28 +0700

    Create CPP0426 - SẮP XẾP XEN KẼ - 2.cpp

---
+++ CPP0426 - SẮP XẾP XEN KẼ - 2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+#define endl '\n'
+using namespace std;
+
+void TestCase()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (auto &x : a) cin >> x;
+    sort(a, a + n, greater<int>());
+    int i = 0, j = n - 1;
+    while (i < j) {
+        cout << a[i++] << " " << a[j--] << " ";
+    }
+    if (i == j) cout << a[i];
+    cout << endl;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--) TestCase();
+    return 0;
+}

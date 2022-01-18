9b7aacfdcaa907e277ff2f0d71a7d31aca84bf41
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-01-18 12:34:46 +0700

    Create CPP0153 - CHIA DƯ TỪ 1 ĐẾN N.cpp

---
+++ CPP0153 - CHIA DƯ TỪ 1 ĐẾN N.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h>
+#include<iomanip>
+
+using namespace std;
+
+void inraketqua(){
+	long long n, k;
+	cin >> n >> k;
+	long long sum = 0;
+	for(int i = 1; i <= n; i++){
+		int d = i % k;
+		sum += d;
+	}
+	cout << sum << endl;
+}
+
+int main(){
+	int k;
+	cin >> k;
+	while(k--){
+		inraketqua();
+	}
+	return 0;
+}

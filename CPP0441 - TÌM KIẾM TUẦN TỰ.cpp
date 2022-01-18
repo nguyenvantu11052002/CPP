8bd00e2da8f782be2ad34f826ff5199a27857410
nguyenvantu11052002 <86882000+nguyenvantu11052002@users.noreply.github.com>
2022-01-18 23:26:42 +0700

    Create CPP0441 - TÌM KIẾM TUẦN TỰ.cpp

---
+++ CPP0441 - TÌM KIẾM TUẦN TỰ.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+#include<math.h>
+#include<algorithm>
+using namespace std;
+
+void inraketqua(){
+	int n, x;
+	cin >> n >> x;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	for(int i = 0; i < n; i++){
+		if(a[i] == x){
+			cout << i + 1 << endl;
+			return;
+		}
+	}
+	cout << -1 << endl;
+}
+
+int main(){
+	int k;
+	cin >> k;
+	while(k--){
+		inraketqua();
+	}
+	return 0;
+}
